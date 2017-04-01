e85b3e9b3b076f9fbda3b7ef663d47068fba8ee8
timothy kurniawan <timothykurniawan@gmail.com>
2017-04-02 04:23:40 +0800

    add slider and simple plot

--- PURe/app/src/main/java/com/example/pure/pure/MainActivity.java
+++ PURe/app/src/main/java/com/example/pure/pure/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.pure.pure;
 
 import android.os.Bundle;
+import android.graphics.*;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private XYPlot uviPlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +33,61 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        uviPlot = (XYPlot) findViewById(R.id.uvi_plot);
+
+        final Number[] domainLabels = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+        Number[] uvIndex = {0, 2, 5, 6, 10, 9, 11, 8, 1, 1, 0};
+
+        XYSeries uviSeries = new SimpleXYSeries(
+                Arrays.asList(uvIndex),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+                "uv-index"
+        );
+
+        Paint lineFill = new Paint();
+        lineFill.setAlpha(200);
+        lineFill.setShader(new LinearGradient(0, 0, 0, 250, Color.WHITE, Color.GREEN, Shader.TileMode.MIRROR));
+
+        LineAndPointFormatter uviSeriesFormatter = new LineAndPointFormatter();
+        uviSeriesFormatter.setFillPaint(lineFill);
+        uviSeriesFormatter.setInterpolationParams(
+                new CatmullRomInterpolator.Params(20, CatmullRomInterpolator.Type.Centripetal));
+
+        uviPlot.addSeries(uviSeries, uviSeriesFormatter);
+        uviPlot.setDomainLabel("time");
+        uviPlot.setRangeLabel("uv index");
+        uviPlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round(((Number) obj).floatValue());
+                if (i % 3 == 0)
+                    return toAppendTo.append(domainLabels[i]);
+                else return toAppendTo.append("");
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+        uviPlot.setRangeBoundaries(0, 13, BoundaryMode.FIXED);
+        uviPlot.setRangeStep(StepMode.INCREMENT_BY_VAL, 3);
+
+        final Button buttonUVI = (Button) findViewById(R.id.uvi_level);
+
+        buttonUVI.setOnClickListener(new View.OnClickListener() {
+            LinearLayoutCompat uviMainContentLayout = (LinearLayoutCompat) findViewById(R.id.uvi_main_content);
+            LinearLayoutCompat.LayoutParams uviMainContentLayoutParams = (LinearLayoutCompat.LayoutParams) uviMainContentLayout.getLayoutParams();
+            boolean isChecked = false;
+            @Override
+            public void onClick(View v) {
+                if (!isChecked) {
+                    uviMainContentLayoutParams.height = LinearLayoutCompat.LayoutParams.WRAP_CONTENT;
+                } else {
+                    uviMainContentLayoutParams.height = 0;
+                }
+
+                isChecked = !isChecked;
+                v.requestLayout();
             }
         });
     }
